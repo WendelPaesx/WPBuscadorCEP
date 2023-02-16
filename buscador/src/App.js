f844af0795c7b2f59f54c374395ae88cff4f9dfe
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2023-02-16 02:14:22 -0300

    .

---
+++ buscador/src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="container">
+           <h1 className="titulo">Buscador CEP</h1>
+           <div className="containerInput">
+            <input type="text" placeholder="Digite seu CEP"></input>
+
+           </div>
+           <button>
+            Buscar
+           </button>
+
+    </div>
+  );
+}
+
+export default App;
